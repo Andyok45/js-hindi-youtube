83055ac432664616eba1d23b3fe0b57eaa695fc0
Andyok45 <132658729+Andyok45@users.noreply.github.com>
2023-09-28 03:42:46 +0000

    Datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,3 +21,16 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
+
+// ***************** Operations *****************
+
+let value = 3 
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " ansh"
+
+let str3 = str1 + str2
+console.log(str3);
+
