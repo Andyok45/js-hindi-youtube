1f84e185a99dd2768dcf40e0c998dcb4bdc58f55
Andyok45 <132658729+Andyok45@users.noreply.github.com>
2023-09-28 09:04:27 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5] 
+const myArr2 = new Array(1, 2, 3, 4, 5)
+
+// Array methods 
+
+myArr.push(6)
+// adds the number 6 in the array at the last
+myArr.push(7)
+myArr.pop()
+// removes the last element from the array
+
+myArr.unshift(9)
+// to add 9 in starting of the array
+myArr.shift()
+// removes the starting element of the array
+
+console.log(myArr.includes(9));
+// output typeof boolean here the output will be false
+console.log(myArr.indexOf(3));
+// shows index if the number is not in the array the output will be -1 
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(newArr);
+// join changes the type from array to string 
+
+// slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+// slices the array from 1st index to 3rd index including the 1st and second and not the third
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+// splice includes 1st 2nd and 3rd index , the original array gets manipulated and the array remains the elements that are not spliced
+console.log(myn2);
+
+
+
---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,35 @@
+const marvel_heroes = ["thor", "Ironman", "Spiderman"]
+const dc_heroes = ["superman", "flash", "batman"]
+
+marvel_heroes.push(dc_heroes)
+// adds the dc_heroes array as an element in marvel_heroes array 
+
+console.log(marvel_heroes);
+
+const allHeroes = marvel_heroes.concat(dc_heroes)
+console.log(all_heroes);
+// merges two arrays and makes a copy of it
+
+const all_new_heroes = [...marvel_heroes, ...dc_heroes]
+
+console.log(all_new_heroes);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+// combines all the array in array and makes a single array
+console.log(real_another_array);
+
+console.log(Array.from("Hitesh"))
+// makes an array from the string
+
+console.log(Array.from({name: "Ansh"}))
+// interesting case as the output shows empty array since we did not define that the array has to be formed from the keys or the values
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+
