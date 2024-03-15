a0860325efdbb3189385c3d6e805309768f29baa
Andyok45 <132658729+Andyok45@users.noreply.github.com>
2024-03-15 15:06:02 +0530

    maps

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -21,3 +21,14 @@ map.set('FR', "France")
 // maps adds unique values and does not repeat the same values and values are displayed in order
 console.log(map)
 
+for( const key of map ){
+    console.log(key);      // this will output each key and value as a paired array 
+}
+
+for( const [key, value] of map){
+    console.log(key, ':-', value);  // this will output as a string -> IN :- India
+}                                                                   // USA :- United States Of America 
+                                                                    // Fr :- France
+
+
+// for objects we can not use the way we did in map                                                                    
