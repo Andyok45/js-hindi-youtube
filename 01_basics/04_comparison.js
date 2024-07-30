140cdf454148c7eba00ece4b5a52b7adb2da0116
Andyok45 <132658729+Andyok45@users.noreply.github.com>
2024-07-30 17:59:52 +0530

    Update 04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -4,6 +4,9 @@
 // console.log(2 == 1);
 // console.log(2 != 1);
 
+// console.log("2" > 1);   // output -> true  (automatically converts string to num)
+// console.log("02" > 1);  // output -> true 
+
 console.log(null > 0);   
 console.log(null == 0);
 console.log(null >= 0);
@@ -12,3 +15,7 @@ console.log(null >= 0);
 // the reason is that an equality check == and comparisons > < >= <= work differently. 
 // comparisons convert null to a number, treating it as 0. That's why (3) null >= 0 id true and (1) null > 0 is false
 // === strict check -> checks datatype
+
+// console.log(undefined == 0);   }
+// console.log(undefined > 0);    }   all gives false  
+// console.log(undefined < 0);    }
