2a19335edef8ebc78db4706d0e7dbbcd53ab9565
Andyok45 <132658729+Andyok45@users.noreply.github.com>
2024-07-30 17:25:19 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,23 +23,34 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+console.log(stringNumber)           // 33
+console.log(typeof stringNumber);   // string
 
 // ***************** Operations *****************
 
 let value = 3 
 let negValue = -value
-console.log(negValue)
+console.log(negValue)  // -3
 
 let str1 = "hello"
 let str2 = " ansh"
 
 let str3 = str1 + str2
-console.log(str3);
+console.log(str3);      // hello ansh
 
 console.log("1" + 2)        // output 12       
 console.log(1 + "2")        // output 12
 console.log("1" + 2 + 2)    // output 122  
 console.log(1 + 2 + "2")    // output 32   
 
+console.log(+true);         // output 1
+console.log(+"");           // output 0
+
+let gameCounter = 100;
+// ++gameCounter;    output 101   // prefix... used before putting the value 
+// gameCounter++;    output 101   // postfix... used after putting the value 
+/////////////////////////// Eg of prefix and postfix////////////////////////
+// x = 3;   y = x++;        output -> x = 4 , y = 3
+// x = 3;   y = ++x;        output -> x = 4 , y = 4
+
+console.log(gameCounter);
