eff8da4d535b038f4ae81b06863d8a69154e7f64
Andyok45 <132658729+Andyok45@users.noreply.github.com>
2024-08-02 15:57:15 +0530

    Update 02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -16,11 +16,11 @@ console.log(all_new_heroes);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
+const real_another_array = another_array.flat(Infinity) // infinity defines that till infintiy depth the numbers must be flat 
 // combines all the array in array and makes a single array
 console.log(real_another_array);
 
-console.log(Array.from("Hitesh"))
+console.log(Array.from("Hitesh")) // [ 'H', 'i', 't', 'e', 's', 'h' ]
 // makes an array from the string
 
 console.log(Array.from({name: "Ansh"}))
@@ -30,6 +30,6 @@ let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3)); // [ 100, 200, 300 ]
 
 
